4351a8059a3a92e55c45a86cf863204cfa18f501
Angelo Bazo <93289318+AngeloBazo@users.noreply.github.com>
2023-03-14 11:49:10 -0300

    Commit em 14-03-23

--- curso-java-intermediario-Loiane-Groner/src/com/loiane/cursojava/aula74/ExercicioSemaforoMain.java
+++ curso-java-intermediario-Loiane-Groner/src/com/loiane/cursojava/aula74/ExercicioSemaforoMain.java
@@ -6,8 +6,20 @@ public class ExercicioSemaforoMain {
 
 	public static void main(String[] args) {
 
+		for (int i=0; i<2; i++) {
+		
+		System.out.println(">>> Início do ciclo:");
+		System.out.println();
+			
 		ExercicioSemaforoClasse thread1 = new ExercicioSemaforoClasse("*** VERDE ***", 5000);
 		Thread t1 = new Thread(thread1);
+
+		ExercicioSemaforoClasse thread2 = new ExercicioSemaforoClasse("*** Amarelo ***", 1000);
+		Thread t2 = new Thread(thread2);
+
+		ExercicioSemaforoClasse thread3 = new ExercicioSemaforoClasse("*** VERMELHO ***", 5000);
+		Thread t3 = new Thread(thread3);
+
 		t1.start();
 		try {
 			t1.join();
@@ -15,8 +27,6 @@ public class ExercicioSemaforoMain {
 			e.printStackTrace();
 		}
 
-		ExercicioSemaforoClasse thread2 = new ExercicioSemaforoClasse("*** Amarelo ***", 1000);
-		Thread t2 = new Thread(thread2);
 		t2.start();
 		try {
 			t2.join();
@@ -24,14 +34,20 @@ public class ExercicioSemaforoMain {
 			e.printStackTrace();
 		}
 
-		ExercicioSemaforoClasse thread3 = new ExercicioSemaforoClasse("*** VERMELHO ***", 5000);
-		Thread t3 = new Thread(thread3);
 		t3.start();
 		try {
 			t3.join();
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
+
+		System.out.println("//---- Fim do ciclo ----\\");
+		System.out.println();
+		
+		}
+	
+		System.out.println("FIM DO MAIN!!!");
+	
 	}
 
 }
