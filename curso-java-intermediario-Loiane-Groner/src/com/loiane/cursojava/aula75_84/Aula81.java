72e9cf5d1e120e2b4662ae2a3991891b745997bd
Angelo Bazo <93289318+AngeloBazo@users.noreply.github.com>
2023-04-05 15:16:18 -0300

    Commit em 05-04-23

---
+++ curso-java-intermediario-Loiane-Groner/src/com/loiane/cursojava/aula75_84/Aula81.java
@@ -0,0 +1,17 @@
+package com.loiane.cursojava.aula75_84;
+
+public class Aula81 {
+
+	public static void main(String[] args) {
+
+		String teste = "Teste";
+
+		System.out.println(teste);
+
+		System.out.println(teste.toLowerCase());
+
+		System.out.println(teste.toUpperCase());
+
+	}
+
+}
