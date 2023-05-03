6b65b5f654fa16c55654cbec3dd90ad6de7d4f9f
Angelo Bazo <93289318+AngeloBazo@users.noreply.github.com>
2023-05-03 15:56:05 -0300

    Commit em 03-05-23

---
+++ curso-java-intermediario-Loiane-Groner/src/com/loiane/cursojava/aula85_100/Aula95ResourceBundle.java
@@ -0,0 +1,16 @@
+package com.loiane.cursojava.aula85_100;
+
+import java.util.Locale;
+
+public class Aula95ResourceBundle {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("Locale atual " + Locale.getDefault());
+		
+		
+
+	}
+
+}
